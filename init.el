51c6b3502ac09d63348656ce94ef1afc1d8bfde9
yamad <jyamada1@gmail.com>
2013-08-29 14:57:50 -0400

    Unfill paragraph and region functions to reverse fill-* fns

--- init.el
+++ init.el
@@ -116,6 +116,23 @@
 (load-theme 'zenburn t)
 (global-font-lock-mode 1)
 
+;; Unfill functions (opposes fill-paragraph and fill-region)
+(defun unfill-paragraph ()
+  "Takes a multi-line paragraph and makes it into a single line
+of text"
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+(defun unfill-region (beg end)
+  "Unfill the region, joining text paragraphs into a single
+    logical line.  This is useful, e.g., for use with
+    `visual-line-mode'."
+  (interactive "*r")
+  (let ((fill-column (point-max)))
+    (fill-region beg end)))
+(define-key global-map "\M-Q" 'unfill-paragraph)
+(define-key global-map "\C-\M-Q" 'unfill-region)
+
 ;; Set tab behavior
 (setq-default indent-tabs-mode nil)
 (setq-default c-basic-offset 4)
