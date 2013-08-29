272495d69e2dcc95f58ed610514315b6ffbe095e
yamad <jyamada1@gmail.com>
2013-08-29 15:00:50 -0400

    Gnus (news) configuration

--- init.el
+++ init.el
@@ -148,6 +148,21 @@ of text"
 (require 'ert)
 (require 'ert-x)
 
+;; News
+(setq gnus-select-method '(nntp "news.eternal-september.org"))
+(setq gnus-secondary-select-methods '((nntp "news.gmane.org")))
+(setq user-full-name "Jason Yamada-Hanff")
+(setq user-mail-address "jyamada1@gmail.com")
+
+(setq gnus-summary-line-format "%O%U%R%z%d %B%(%[%4L: %-22,22f%]%) %s\n")
+(setq gnus-summary-mode-line-format "Gnus: %p [%A / Sc:%4z] %Z")
+(setq gnus-summary-same-subject "")
+(setq gnus-sum-thread-tree-root "")
+(setq gnus-sum-thread-tree-single-indent "")
+(setq gnus-sum-thread-tree-leaf-with-other "+-> ")
+(setq gnus-sum-thread-tree-vertical "|")
+(setq gnus-sum-thread-tree-single-leaf "`-> ")
+
 ;; YASnippet
 ;; ======================================
 (require 'yasnippet)
