5a726e9b16e70173dbc0a9ba70d79efac46fb628
yamad <jyamada1@gmail.com>
2016-07-03 17:11:21 -0700

    clear out old custom variables

--- init-custom.el
+++ init-custom.el
@@ -10,9 +10,8 @@
  '(canlock-password "17fbb2e43ad75165c6b69f83ed8c0a5f34d16270")
  '(custom-safe-themes
    (quote
-    ("40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" "cd70962b469931807533f5ab78293e901253f5eeb133a46c2965359f23bfb2ea" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" default)))
+    ("1db337246ebc9c083be0d728f8d20913a0f46edc0a00277746ba411c149d7fe5" "aae95fc700f9f7ff70efbc294fc7367376aa9456356ae36ec234751040ed9168" "40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" "cd70962b469931807533f5ab78293e901253f5eeb133a46c2965359f23bfb2ea" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" default)))
  '(ecb-options-version "2.40")
- '(fci-rule-color "#383838")
  '(frame-background-mode (quote dark))
  '(nrepl-message-colors
    (quote
@@ -40,9 +39,3 @@
      (340 . "#94BFF3")
      (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
