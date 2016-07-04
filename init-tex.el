5cbb1ac48306b74854b5c062b33064405d1df2d7
yamad <jyamada1@gmail.com>
2016-07-03 23:01:10 -0700

    remove deprecated custom biber support for AuCTeX

--- init-tex.el
+++ init-tex.el
@@ -135,38 +135,3 @@
   (add-hook 'ConTeXt-mode-hook
             '(lambda ()
                (local-set-key "$" 'start-context-math))))
-
-
-;; add biber support. no longer needed
-;; TODO: delete this
-(eval-after-load "tex"
-  '(add-to-list 'TeX-command-list
-                '("Biber" "biber %s" TeX-run-Biber nil t :help "Run Biber") t))
-
-(defun TeX-run-Biber (name command file)
-  "Create a process for NAME using COMMAND to format FILE with Biber."
-  (let ((process (TeX-run-command name command file)))
-    (setq TeX-sentinel-function 'TeX-Biber-sentinel)
-    (if TeX-process-asynchronous
-        process
-      (TeX-synchronous-sentinel name file process))))
-
-(defun TeX-Biber-sentinel (process name)
-  "Cleanup TeX output buffer after running Biber."
-  (goto-char (point-max))
-  (cond
-   ;; Check whether Biber reports any warnings or errors.
-   ((re-search-backward (concat
-                         "^(There \\(?:was\\|were\\) \\([0-9]+\\) "
-                         "\\(warnings?\\|error messages?\\))") nil t)
-    ;; Tell the user their number so that she sees whether the
-    ;; situation is getting better or worse.
-    (message (concat "Biber finished with %s %s. "
-                     "Type `%s' to display output.")
-             (match-string 1) (match-string 2)
-             (substitute-command-keys
-              "\\\\[TeX-recenter-output-buffer]")))
-   (t
-    (message (concat "Biber finished successfully. "
-                     "Run LaTeX again to get citations right."))))
-  (setq TeX-command-next TeX-command-default))
