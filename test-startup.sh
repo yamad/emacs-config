91bec5b4925adddca1f6e7a06f982f7886ce3669
yamad <jyamada1@gmail.com>
2017-04-25 23:56:58 -0700

    add Steve Purcell's startup test code

---
+++ test-startup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+# Test emacs startup files, by Steve Purcell
+# https://github.com/purcell/emacs.d/
+# see licenses/purcell
+
+if [ -n "$TRAVIS" ]; then
+    # Make it look like this is ~/.emacs.d (needed for Emacs 24.3, at least)
+    export HOME=$PWD/..
+    ln -s emacs.d ../.emacs.d
+fi
+echo "Attempting startup..."
+${EMACS:=emacs} -nw --batch \
+                --eval '(let ((debug-on-error t)
+                              (url-show-status nil)
+                              (user-emacs-directory default-directory)
+                              (package-user-dir (expand-file-name (concat "elpa-" emacs-version)))
+                              (user-init-file (expand-file-name "init.el"))
+                              (load-path (delq default-directory load-path)))
+                           (load-file user-init-file)
+                           (run-hooks (quote after-init-hook)))'
+echo "Startup successful"
