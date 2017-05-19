048575303b46e70bff05b23e2dcda7cd0b54f0e2
yamad <jyamada1@gmail.com>
2017-05-19 00:37:06 -0700

    speed up C mode

--- init.d/init-lang.el
+++ init.d/init-lang.el
@@ -11,37 +11,35 @@
 
 (use-package cc-mode
   :defer t
-  :ensure nil                           ; built-in
-  :config
+  :preface
+  (defun c-lineup-arglist-tabs-only (ignored)
+    "Line up argument lists by tabs, not spaces"
+    (let* ((anchor (c-langelem-pos c-syntactic-element))
+           (column (c-langelem-2nd-pos c-syntactic-element))
+           (offset (- (1+ column) anchor))
+           (steps (floor offset c-basic-offset)))
+      (* (max steps 1)
+         c-basic-offset)))
+
+  (defun my-make-CR-do-indent ()
+    (define-key c-mode-base-map "\C-m" 'c-context-line-break))
+
+  :init
   (setq c-default-style '((c-mode . "linux")))
   (add-hook 'c-mode-common-hook #'hs-minor-mode) ; hide/show mode
-  )
+  (add-hook 'c-initialization-hook 'my-make-CR-do-indent)
 
-(use-package company-c-headers
-  :after cc-mode
-  :init
-  (jyh-company-for-mode 'c-mode-common-hook company-c-headers))
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              ;; Add kernel style
+              (c-add-style
+               "linux-tabs-only"
+               '("linux" (c-offsets-alist
+                          (arglist-cont-nonempty
+                           c-lineup-gcc-asm-reg
+                           c-lineup-arglist-tabs-only))))))
 
-(defun c-lineup-arglist-tabs-only (ignored)
-  "Line up argument lists by tabs, not spaces"
-  (let* ((anchor (c-langelem-pos c-syntactic-element))
-         (column (c-langelem-2nd-pos c-syntactic-element))
-         (offset (- (1+ column) anchor))
-         (steps (floor offset c-basic-offset)))
-    (* (max steps 1)
-       c-basic-offset)))
-
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            ;; Add kernel style
-            (c-add-style
-             "linux-tabs-only"
-             '("linux" (c-offsets-alist
-                        (arglist-cont-nonempty
-                         c-lineup-gcc-asm-reg
-                         c-lineup-arglist-tabs-only))))))
-
-(add-hook 'c-mode-hook
+  (add-hook 'c-mode-hook
           (lambda ()
             (let ((filename (buffer-file-name)))
               ;; Enable kernel mode for the appropriate files
@@ -51,11 +49,12 @@
               )
             (setq indent-tabs-mode t)
             (setq show-trailing-whitespace t)
-            (c-set-style "linux")))
+            (c-set-style "linux"))))
 
-(defun my-make-CR-do-indent ()
-  (define-key c-mode-base-map "\C-m" 'c-context-line-break))
-(add-hook 'c-initialization-hook 'my-make-CR-do-indent)
+(use-package company-c-headers
+  :after cc-mode
+  :init
+  (jyh-company-for-mode 'c-mode-common-hook company-c-headers))
 
 (use-package rtags
   :ensure t
